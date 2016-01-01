a87919e5fbb9c2a4cb72a0a94321d52dc1e9c280
Tian Xia <SummerRainET2008@gmail.com>
2015-12-31 19:43:10 -0500

    new file:   water.ubuntu.sh

---
+++ water.ubuntu.sh
@@ -0,0 +1,213 @@
+# date = 2012-1-14 
+
+# stateful programmatic web browsing
+   sudo apt-get install python-mechanize
+
+#  sudo update-grub
+
+#  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/w004txx/install/boost_1_42_0/stage/lib
+#  动态库在链接时，除了-L要加路径之外，这个路径也要加。
+
+# shogun-python 大规模机器学习工具箱。
+# shogun-octave - Large Scale Machine Learning Toolbox
+#  PyBrain is a modular Machine Learning Library for Python
+#  SymPy is a symbolic manipulation package, written in pure Python
+#  CVXOPT, Python Software for Convex Optimization
+#  CVXMOD, lingo-like programming pack 
+   sudo apt-get install shogun-python*
+   sudo apt-get install shogun-octave*
+   sudo apt-get install python-scipy
+
+# 命令行浏览器
+   sudo apt-get install w3m
+
+# 文件转换，windows <==> linux
+# fromdos todos
+   sudo apt-get install tofrodos
+
+# Eclim可以将eclipse嵌入到vim中。
+
+# 播放软件，前端软件smplayer特别牛逼，不建议用电影播放器。
+   sudo apt-get install mplayer
+
+# e-mail
+   sudo apt-get install thunderbird
+
+# 虚拟软件，必须安装中文字体。
+   sudo apt-get install wine
+
+# MSN终端
+   sudo apt-get install emesene
+
+# 编译器
+   sudo apt-get install g++ 
+   sudo apt-get install python2.7
+
+# enhanced python shell: bpython 
+   sudo apt-get install bpython
+
+# 必须安装python-profiler才可以import cProfile。
+   sudo apt-get install python-profiler
+
+
+# 调试器
+   sudo apt-get install ddd 
+   sudo apt-get install gdb 
+
+# 数学软件
+   sudo apt-get install octave3.2 
+   sudo apt-get install qtoctave
+
+# 安装小鱼谚语。
+   sudo apt-get install fortune
+
+# 挂载iso文件：mount -t iso9660 -o loop xxx.iso /path
+# 拷贝光盘：cp /dev/cdrom xxx.iso 
+
+# 辞典: stardict
+# 安装辞典：将辞典文件拷贝到/usr/share/stardict/dic下。
+# 辞典网址：http://stardict.sourceforge.net/Dictionaries_zh_CN.php
+   sudo apt-get install stardict
+   sudo apt-get install sdcv
+
+# 将rpm转化为deb, sudo alien XXX.rpm
+   sudo apt-get install alien
+
+# 增加右键打开
+   sudo apt-get install nautilus-open-terminal
+
+#  装c++的帮助包
+   sudo apt-get install manpages-dev
+
+# apt: /etc/apt/sources.list
+# apt软件地址/var/cache/apt/archives
+
+# 转化目录和文件的编码:convmv -f utf-8 -t gbk * --notest
+   sudo apt-get install convmv
+
+# ctags支持函数查看,自带于vim-full;taglist可视化查看；pythonist可以提示。
+   sudo apt-get install vim-full 
+   sudo apt-get install exuberant-ctags 
+
+# 测试文件编码:enca
+   sudo apt-get install enca
+
+# 在实验室安装打印机:IP: 10.28.0.119
+
+# 分区工具:gparted
+   sudo apt-get install gparted
+
+# 安装latex必须安装
+   sudo apt-get install kile 
+   sudo apt-get install lyx 
+   sudo apt-get install texlive-science
+   sudo apt-get install latexdraw   # drawing editor
+
+# 安装nerolinux, 增加源"deb ftp://202.116.65.70/pub/ubuntu jaunty main"
+
+# 截图软件 
+# 使用命令为scrot -sb
+   sudo apt-get install scrot
+
+# 多线程下载工具：axel；另一个好用高效的工具：wget。
+   sudo apt-get install axel 
+   sudo apt-get install wget
+
+# 查看chm文件。
+   sudo apt-get install chmsee
+
+# optimal font = nimbus mono L
+
+# 安装独立显卡的软件：
+# sudo apt-get install envyng*
+
+# 修改~/.config/user-dirs.dirs文件，可以修改主文件夹中的中英文文件夹
+
+# pdf以及给pdf批注的软件
+   sudo apt-get install okular
+
+# 使用awk提取短语表src、tgt命令：
+# cat phrase.table | awk -F" \|\|\| " '{print $1}' 或者 awk -F" \|\|\| " '{print $1}' phrase.table
+
+# 使用find命令： find . -iregex ".*hello.*" -exec ls -lh {} \;
+
+# 正则表达式grep，是perl语法的一个子集，但正则表达式要加双引号。
+# ls | grep ".*\.c[a-z]?"
+
+# 查看硬件配置命令：lspci -v，以及lsmod | grep snd
+
+# 查看开机时间: uptime
+
+# 查看所有硬盘UUID：sudo blkid
+
+# doxygen - C, C++, Java, Python 和其他语言的文档系统。
+   sudo apt-get install doxygen
+
+# 屏幕录像软件:
+   sudo apt-get install xvidcap
+
+# 安装 JAVA 环境支持  
+   sudo apt-get install jamvm
+
+# UML 建模软件
+   sudo apt-get install umbrello
+
+# 安装 RAR zip 压缩/解压缩程序
+   sudo apt-get install rar
+   sudo apt-get install unrar 
+   sudo apt-get install p7zip    
+
+# 电驴工具:
+# sudo apt-get install amule amule-common amule-utils
+
+# FTP 客户端 gFTP
+   sudo apt-get install gftp 
+   sudo apt-get install filezilla
+
+# 数据库软件:
+   #sudo apt-get install mysql-admin mysql-client mysql-gui-tools-common mysql-query-browser mysql-server
+
+# 安装osd
+   sudo apt-get install gnome-osd
+
+# cooliris, add-ons of firefox, 3D.
+
+# proxychains, usage; sudo proxychains apt-get update
+   sudo apt-get install proxychains
+   # usage: sudo proxychains apt-get install bpython
+   
+# tray all
+   sudo apt-get install alltray
+
+# DownloadHelper 4.7.3, add-ons of downloadin video
+   
+# convert VCD to other video format.
+   sudo apt-get install winff
+   sudo apt-get install arista
+   sudo apt-get install soundconverter
+
+# guitar pro software in linux.
+   sudo apt-get install tuxguitar 
+
+# dictionary for synonyms.
+   sudo apt-get install aiksaurus  
+   
+# 北京外语广播 mms://alive.bjradio.com.cn/am774
+
+# 终端调用beep发声。alsamixer，确保pc beep中出现OO，使用m调整。
+
+# guitar节拍器
+   sudo apt-get install gtick
+
+# 分屏共享鼠标和键盘。在server端要建立配置文件，只填写桌面名称，不用填写ip地址。 
+   #sudo apt-get install synergy
+
+# 安装ssh服务
+   sudo apt-get install ssh
+
+# 修改ibus自定义辞典，vim /home/et/.config/ibus/pinyin/phrases.txt
+
+# 加快ssh响应时间，将GSSAPIAuthentication yes注视掉。
+
+# 安装lz = libz
+   sudo apt-get install libz-dev
