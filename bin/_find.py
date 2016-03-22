c1da959de820ed95b6d2f5653d2e8e26a7daacc8
Tian Xia <SummerRainET2008@gmail.com>
2016-03-22 12:21:01 -0400

    modified:   bin/_find.py   modified:   bookmark.text

--- bin/_find.py
+++ bin/_find.py
@@ -9,13 +9,29 @@ if __name__ == "__main__":
   parser = optparse.OptionParser(usage = "cmd [optons] kw1 kw2 ...]")
   #parser.add_option("-q", "--quiet", action = "store_true", dest = "verbose",
                      #default = False, help = "")
+  parser.add_option("-d", "--dir", action = "store_true", dest = "search_dir",
+                     default = False, help = "")
+  parser.add_option("-g", "--global", action = "store_true", dest = "global_search",
+                     default = False, help = "")
   (options, args) = parser.parse_args()
 
   if len(args) > 0:
+    if options.global_search:
+      os.chdir("/Users/world/inf")
+
     args = [kw.lower() for kw in args]
-    cmd = ('''find . -iregex ".*%s.*" %s'''
-           %(args[0], " ".join(["| grep -i %s" %kw for kw in args[1:]])))
+    search_dir = "-type d" if options.search_dir else ""
+    cmd = ('''find . %s -iregex ".*%s.*" %s'''
+           %(search_dir, args[0], 
+             " ".join(["| grep -i %s" %kw for kw in args[1:]])))
     #print cmd
-    os.system(cmd)
+    files = []
+    for f in os.popen(cmd):
+      if ".git" in f or "/." in f:
+        continue
+      files.append(f.strip())
+
+    print "\n".join(files)
+
 
 
