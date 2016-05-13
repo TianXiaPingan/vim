990d1a95d5c37a2b0f3f5f00aa840ab67d7073a2
Tian Xia <txia@godaddy.com>
2016-05-13 09:49:15 -0700

    bin/_tag_file --> /Users/txia/inf/study/experiments/FileTags/bin
      new file:   bin/_tag_file
      modified:   bin/_tag_file.py

--- bin/_tag_file.py
+++ bin/_tag_file.py
@@ -4,35 +4,49 @@ from algorithm import *
 import subprocess
 
 debug = False
+#debug = True
 
-def write_file_tag(fn, tags):
-  # writes the list of tags to three xattr fields on a file-by file basis:
-  # kMDItemFinderComment, _kMDItemUserTags, kMDItemOMUserTags.
-  # Uses subprocess instead of xattr module. Slower but no dependencies.
-  # To check if it worked, use xattr -l FileName 
+class FileTag:
+  def __init__(self, fn):
+    self._fn = fn
 
-  plistFront = '''
-  <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" \
-    "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-    <array>'''
-  plistEnd = '''</array></plist>'''
+  def read_tags(self):
+    return []
 
-  plistTagString = " ".join(["<string> %s </string>" %tag for tag in tags])
-  tag_text = (plistFront + plistTagString + plistEnd).encode("utf8")
+  def append_tags(self, tags):
+    # tags: a list
+    tags = self.read_tags() + tags
+    self._write_tags(tags)
 
-  opt_tag = "com.apple.metadata:"
-  XattrList = ["kMDItemFinderComment", "_kMDItemUserTags", "kMDItemOMUserTags"]
+  def _write_tags(self, tags):
+    # writes the list of tags to three xattr fields on a file-by file basis:
+    # [kMDItemFinderComment, _kMDItemUserTags, kMDItemOMUserTags].
+    # Uses subprocess instead of xattr module. Slower but no dependencies.
+    # To check if it worked, use xattr -lx FileName 
 
-  result = ""
-  for field in XattrList:  
-    xattr_cmd = '''xattr -w %s '%s' "%s"''' %(opt_tag + field, tag_text, fn)
-    if debug:
-      print xattr_cmd
-    r = subprocess.check_output(xattr_cmd, stderr = subprocess.STDOUT, 
-                                shell = True) 
-    result += r
-  return result
+    plist_tpt = '''<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" \
+      "http://www.apple.com/DTDs/PropertyList-1.0.dtd"> \
+      <plist version="1.0"> <array> %s </array> </plist> '''
+
+    plist_tags = " ".join(["<string> %s </string>" %tag for tag in tags])
+    plist_text = (plist_tpt %plist_tags).encode("utf8")
+
+    opt_tag = "com.apple.metadata:"
+    result = ""
+    for field in [
+                  #"kMDItemFinderComment", 
+                  "_kMDItemUserTags", 
+                  #"kMDItemOMUserTags"
+                 ]:
+      xattr_cmd = '''xattr -w %s '%s' "%s"''' %(opt_tag + field, 
+                                                plist_text, 
+                                                self._fn)
+      if debug:
+        print xattr_cmd, "\n"
+      r = subprocess.check_output(xattr_cmd, shell = True, 
+                                  stderr = subprocess.STDOUT)
+      result += r
+    return result
 
 if __name__ == "__main__":
   parser = optparse.OptionParser(usage = "cmd [optons] 'tag1 tag2 ...'" 
@@ -43,5 +57,6 @@ if __name__ == "__main__":
   if len(args) >= 2:
     tags = args[0].split() 
     for f in args[1:]: 
-      write_file_tag(f, tags)
+      file_tag = FileTag(f)
+      file_tag.append_tags(tags)
       print "%s is done" %f     
