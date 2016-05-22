13ec767e382a8ff9a1987cc85f7d66b0dcfda187
Tian Xia <SummerRainET2008@gmail.com>
2016-05-22 01:04:09 -0700

    new file:   bin/_grep.py

---
+++ bin/_grep.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+#coding: utf8
+
+from algorithm import *
+
+if __name__ == "__main__":
+  os.system("clear")
+
+  parser = optparse.OptionParser(usage = "cmd [optons] kw]")
+  #parser.add_option("-q", "--quiet", action = "store_true", dest = "verbose",
+                     #default = False, help = "")
+  parser.add_option("-t", dest = "fn_ext",
+                    default = "java|scala|c|cpp|hpp|py", 
+                    help = "default java|scala|c|cpp|hpp|py")
+  (options, args) = parser.parse_args()
+
+  cmd_tpt = r"find . -iregex '.*\.%s' -exec grep -iHn %s {} \;"
+  if len(args) > 0:
+    for fn_ext in options.fn_ext.split("|"):
+      cmd = cmd_tpt %(fn_ext, args[0])  
+      #print cmd
+      os.system(cmd)
+
+
+
