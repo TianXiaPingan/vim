db5c3d60da5f97a7cff5cb2d7a2ebbd1e37f27e6
Tian Xia <txia@godaddy.com>
2016-08-10 14:14:09 -0700

    new file:   bin/_cpRemote.py   deleted:    bin/_uploadToServer.py   modified:   bookmark.text

---
+++ bin/_cpRemote.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+#coding: utf8
+
+from algorithm import *
+
+servers = {
+  "dev1"  : "txia@g1dlfinddev01.dev.glbt1.gdg",
+  "hadoop": "txia@p3plpashl01.prod.phx3.gdg"
+}
+
+def replaceServer(addr):
+  if "@" not in addr:
+    return addr
+  server = addr[: addr.index("@")]
+  return addr.replace(server + "@", servers[server] + ":")
+
+if __name__ == "__main__":
+  parser = optparse.OptionParser(usage = "cmd dev1@dir1 dir2")
+  #parser.add_option("-q", "--quiet", action = "store_true", dest = "verbose",
+                     #default = False, help = "")
+  (options, args) = parser.parse_args()
+
+  assert len(args) == 2
+  
+  srcDir, tgtDir = replaceServer(args[0]), replaceServer(args[1])
+
+  dirOpt = "-r" if srcDir.endswith("/") else ""
+  cmd = "scp %s %s %s" %(dirOpt, srcDir, tgtDir)
+  #print cmd
+  os.system(cmd)
+
