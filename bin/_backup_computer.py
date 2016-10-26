d600d69cd306e4434e040c1a089b17d0a6df6584
Tian Xia <txia@godaddy.com>
2016-10-25 22:15:54 -0700

    modified:   bin/_backup_computer.py   modified:   bookmark.text

--- bin/_backup_computer.py
+++ bin/_backup_computer.py
@@ -8,19 +8,21 @@ if __name__ == "__main__":
   parser.add_option("-d", action = "store_true", dest = "is_delete", 
                     help = "Whether to delete all extra files in the" 
                     "target directory.")
+  parser.add_option("--server", dest = "server", default = "192.168.1.100",
+                    help = "default 192.168.1.100") 
   (options, args) = parser.parse_args()
 
   tpt = "time rsync -ravutzh --progress -e ssh %s %s"
-  server_dir = (r"summer@192.168.1.100:"
-                "'/media/summer/WareHouse/in\ the\ laptop.inf/'")
+  serverDir = ("summer@%s:/media/summer/WareHouse/in-the-laptop.inf/" 
+               %options.server)
 
   os.chdir("/Users/%s/inf" %os.getlogin())
 
   cmd = args[0]
   if cmd == "backup":
-    cmd = tpt %(".", server_dir) 
+    cmd = tpt %(".", serverDir) 
   elif cmd == "restore":
-    cmd = tpt %(server_dir, ".") 
+    cmd = tpt %(serverDir, ".") 
   else:
     print "See help"
     exit(1)
