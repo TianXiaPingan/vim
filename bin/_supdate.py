724d74544a78f9f97081988cf6e39bd511a18208
Tian Xia <txia@godaddy.com>
2017-03-16 22:34:53 -0700

    modified:   bin/_supdate.py     1. Fixed a bug.

--- bin/_supdate.py
+++ bin/_supdate.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
   assert len(args) == 1
   
   tgtDir = replaceServer(args[0])
-  opt = "-d" if options.delete else ""
+  opt = "--delete" if options.delete else ""
   cmd = "rsync -ravutzh --progress -e ssh . %s %s" %(tgtDir, opt)
   print cmd
   os.system(cmd)
