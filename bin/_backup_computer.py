018e14d74c749e8a9f05c4955cf4f858dedd116b
Tian Xia <txia@godaddy.com>
2017-04-06 11:13:25 -0700

    Thu Apr  6 11:13:27 2017

--- bin/_backup_computer.py
+++ bin/_backup_computer.py
@@ -1,33 +1,24 @@
 #!/usr/bin/env python
 
-import os
-import optparse
+from algorithm import *
+
+def isTargetDirValid(path, folder):
+  msg = os.popen("_ssh.py wd 'cd %s; ls'" %path).read()
+  #print msg
+  return folder in msg
 
 if __name__ == "__main__":
-  parser = optparse.OptionParser(usage = "cmd [optons] [backup|restore]]")
-  parser.add_option("-d", action = "store_true", dest = "is_delete", 
-                    help = "Whether to delete all extra files in the" 
+  parser = optparse.OptionParser(usage = "cmd [optons]")
+  parser.add_option("-d", action = "store_true", dest = "delete",
+                    help = "Whether to delete all extra files in the"
                     "target directory.")
-  parser.add_option("--server", dest = "server", default = "192.168.1.104",
-                    help = "default 192.168.1.104") 
   (options, args) = parser.parse_args()
 
-  tpt = "time rsync -ravutzh --progress -e ssh %s %s"
-  serverDir = ("summer@%s:/media/summer/WareHouse/in-the-laptop.inf/" 
-               %options.server)
-
   os.chdir("/Users/%s/inf" %os.getlogin())
+  path, folder = "/media/summer/WareHouse", "in-the-laptop.inf"
+  assert isTargetDirValid(path, folder), path + "/" + folder
 
-  cmd = args[0]
-  if cmd == "backup":
-    cmd = tpt %(".", serverDir) 
-  elif cmd == "restore":
-    cmd = tpt %(serverDir, ".") 
-  else:
-    print "See help"
-    exit(1)
-
-  if options.is_delete:
-    cmd += " --delete"
-  print cmd, "\n"
-  os.system(cmd)
+  cmd = "_supdate.py wd@%s/%s" %(path, folder)
+  if options.delete:
+    cmd += " -d"
+  executeCmd(cmd)
