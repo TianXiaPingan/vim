cc96a27d6dc38a5d118d53e4d4795982d92b15eb
Tian Xia <txia@godaddy.com>
2017-06-16 13:29:31 -0700

    modified:   bin/_supdate.py

--- bin/_supdate.py
+++ bin/_supdate.py
@@ -8,14 +8,19 @@ if __name__ == "__main__":
   parser = optparse.OptionParser(usage = "cmd targetDir")
   #parser.add_option("-q", "--quiet", action = "store_true", dest = "verbose",
                      #default = False, help = "")
+  parser.add_option("--reverse", action = "store_true", dest = "reverse",
+                    default = False)
   parser.add_option("-d", action = "store_true", dest = "delete",
                     default = False)
   (options, args) = parser.parse_args()
   assert len(args) == 1
   
-  tgtDir = replaceServer(args[0])
   opt = "--delete" if options.delete else ""
-  cmd = "rsync -ravutzh --progress -e ssh . %s %s" %(tgtDir, opt)
-  print cmd
-  os.system(cmd)
+  if options.reverse:
+    srcDir = replaceServer(args[0]) + "/"
+    cmd = "rsync -ravutzh --progress -e ssh %s . %s" %(srcDir, opt)
+  else:  
+    tgtDir = replaceServer(args[0])
+    cmd = "rsync -ravutzh --progress -e ssh . %s %s" %(tgtDir, opt)
 
+  executeCmd(cmd)
