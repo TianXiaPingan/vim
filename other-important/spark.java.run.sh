da5acf16fbd907ae3b37265e9184ddc26f46e7d3
Tian Xia <txia@godaddy.com>
2017-07-04 17:22:49 -0700

    Tue Jul  4 17:22:56 2017

---
+++ other-important/spark.java.run.sh
@@ -0,0 +1,11 @@
+/opt/spark/2.1/bin/spark-submit \
+  --master yarn-client \
+  --conf spark.driver.maxResultSize=2G \
+  --conf spark.dynamicAllocation.maxExecutors=200 \
+  --driver-java-options '-Ddata.dir=premium.test' \
+  --driver-memory 10g \
+  --executor-memory 10g  \
+  --class dl.EncoderDecoderLSTM \
+  --jars dl4j-examples-0.8-SNAPSHOT.base.jar \
+  dl4j-examples-0.8-SNAPSHOT.jar -epochNum 100 -useSparkLocal
+
---
+++ other-important/spark.python.run.sh
@@ -0,0 +1,9 @@
+/opt/spark/2.1/bin/spark-submit \
+  --master yarn-client \
+  --conf spark.driver.maxResultSize=5G \
+  --conf spark.dynamicAllocation.maxExecutors=400 \
+  --driver-memory 30g \
+  --executor-memory 8g  \
+  --py-files /home/txia/include/algorithm.py \
+  GenQuerySequence.py
+
