221fb9673aacacaf510e1f337d49ae692b51ef33
Tian Xia <txia@godaddy.com>
2018-01-25 15:49:52 -0800

    modified:   bin/_test_GPU.py

--- bin/_test_GPU.py
+++ bin/_test_GPU.py
@@ -1,24 +1,16 @@
 #!/usr/bin/env python
 
-from theano import function, config, shared, sandbox
-import theano.tensor as T
-import numpy
-import time
+import tensorflow as tf
 
-vlen = 10 * 30 * 768  # 10 x #cores x # threads per core
-iters = 1000
+# Creates a graph.
+with tf.device('/cpu:0'):
+  a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
+  b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
 
-rng = numpy.random.RandomState(22)
-x = shared(numpy.asarray(rng.rand(vlen), config.floatX))
-f = function([], T.exp(x))
-print(f.maker.fgraph.toposort())
-t0 = time.time()
-for i in range(iters):
-  r = f()
-t1 = time.time()
-print("Looping %d times took %f seconds" % (iters, t1 - t0))
-print("Result is %s" % (r,))
-if numpy.any([isinstance(x.op, T.Elemwise) for x in f.maker.fgraph.toposort()]):
-  print('Used the cpu')
-else:
-  print('Used the gpu')
+c = tf.matmul(a, b)
+
+# Creates a session with log_device_placement set to True.
+sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+# Runs the op.
+print(sess.run(c))
+
