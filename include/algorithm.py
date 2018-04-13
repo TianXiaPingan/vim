41ba3436ba7f380a08169c7b717e041efe244075
Tian Xia <summer@Tians-MacBook-Pro.local>
2018-04-12 19:21:16 -0700

    modified:   include/algorithm.py

--- include/algorithm.py
+++ include/algorithm.py
@@ -407,6 +407,11 @@ def renewGSS():
   executeCmd("kinit -R -k -t /home/txia/.ssh/txia.keytab txia@DC1.CORP.GD")
 
 if __name__ == "__main__":
+  parser = optparse.OptionParser(usage = "cmd dev1@dir1 dir2")
+  #parser.add_option("-q", "--quiet", action = "store_true", dest = "verbose",
+  #default = False, help = "")
+  (options, args) = parser.parse_args()
+
   data = createList([10])
   data[0] = 1
   print data
