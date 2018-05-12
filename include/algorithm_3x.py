509fbced70ce982162595665dcecb58a4e378d81
Tian Xia <summer@MacBook-Pro.local>
2018-05-11 20:03:18 -0700

    modified:   include/algorithm_3x.py

--- include/algorithm_3x.py
+++ include/algorithm_3x.py
@@ -39,6 +39,27 @@ except ImportError:
 INF         = float("inf")
 EPSILON     = 1e-6
 
+class TF:
+  @staticmethod
+  def multiHot(x, depth):
+    def funcC(p, v):
+      return tf.less(p, tf.shape(x)[0])
+
+    def funcB(p, v):
+      row = tf.add_n(tf.unstack(indexes[p]))
+      return p + 1, tf.concat([v, [row]], axis=0)
+
+    indexes = tf.one_hot(x, depth)
+    initV = tf.constant(0)
+
+    _, v = tf.while_loop(funcC, funcB,
+                         [initV,
+                          tf.convert_to_tensor([list(range(10))], tf.float32)],
+                         shape_invariants=[initV.get_shape(),
+                                           tf.TensorShape([None, depth])])
+
+    return v[1:,]
+
 class WordIdDict:
   def __init__(self):
     self._word2Id = {}
