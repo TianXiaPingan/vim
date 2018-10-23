1580cbe843d136b56dd152a1cdb90709418926a4
Summer Xia <summer.xia1@pactera.com>
2018-10-23 15:40:19 -0700

    new file:   _setup_git.sh

---
+++ bin/_setup_git.sh
@@ -0,0 +1,8 @@
+git config --global user.name "Summer Xia"
+git config --global user.email "summer.xia1@pactera.com"
+git config --global core.editor "vim"
+git config --global diff.tool vimdiff
+
+# >> git credential-osxkeychain
+# >> git config --global credential.helper osxkeychain
+# >> git config credential.helper store       # in ubuntu
