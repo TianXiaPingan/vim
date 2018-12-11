c1adf3609e92d6e3c5518851f6735f2170d9dcba
Tian Xia <summer@MacBook-Pro-2nd-gen.local>
2018-12-11 15:24:15 -0800

    new file:   bin/_test_cpu_speed.py

---
+++ bin/_test_cpu_speed.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+#coding: utf8
+#author: Tian Xia (summer.xia1@pactera.com)
+
+import time
+import optparse
+
+def run(size):
+  start_time = time.time()
+  result = 0
+  for a in range(size):
+    for b in range(size):
+      for c in range(size):
+        result += a + b + c
+
+  end_time = time.time()
+  print(f"It takes {end_time - start_time: .4f} s")
+  print(f"MacBook Pro: 2.2246 s")
+
+
+if __name__ == "__main__":
+  parser = optparse.OptionParser(usage="cmd [optons] ..]")
+  #parser.add_option("-q", "--quiet", action="store_true", dest="verbose",
+                     #default=False, help="")
+  parser.add_option("--size", default=300)
+  (options, args) = parser.parse_args()
+
+  run(options.size)
+
